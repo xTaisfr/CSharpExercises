25c48319bf4cb97b346eb68c9a7994584ab651b2
TaisFR <tfrtais@gmail.com>
2022-07-14 17:03:25 -0300

    Exercicio 1

--- CSharpExercises/CSharpExercises/Conditionals.cs
+++ CSharpExercises/CSharpExercises/Conditionals.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSharpExercises
 {
-    public class Condicionais
+    public class Conditionals
     {
         ///<summary>
         /// 1) Write a program and ask the user to enter a number. The number should be between 1 to 10. 
@@ -9,7 +9,12 @@
         ///</summary>
         public void Exercise1()
         {
+            Console.WriteLine("Enter a number between 1 to 10");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
 
+            string result = (inputNumber >= 1 && inputNumber <= 10) ? "Valid" : "Invalid";
+
+            Console.WriteLine(result);
         }
     }
 }
